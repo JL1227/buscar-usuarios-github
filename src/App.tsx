7b1c6a6cecb43c03d8aaa9c6d69b7aa686c0f9da
João Lucas <joao.lucas1227_@hotmail.com>
2023-02-08 16:28:36 -0300

    Buscar usuários finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
-import { useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import { useState } from 'react'
+import { FaUser, FaBuilding } from 'react-icons/fa';
+import { RiGitRepositoryFill } from 'react-icons/ri';
+import { IoLocationSharp } from 'react-icons/io5';
 
-type GITHUBResponse = {
+type GitHubUser = {
   name: string;
+  login: string;
   avatar_url: string;
   bio: string;
+  followers: number
+  following: number
+  company: string
+  location: string
 };
 
 function App() {
 
-  const [userName, setUserName] = useState("");
-  const [name, setName] = useState("Aguardando...");
-  const [bio, setBio] = useState("Aguardando...");
-  const [avatarURL, setAvatarURL] = useState("Aguardando...");
+  const [userName, setUserName] = useState<string>("");
+  const [name, setName] = useState<string>("Usuário");
+  const [bio, setBio] = useState<string>("Bio do Usuário");
+  const [avatarURL, setAvatarURL] = useState<string>("./assets/git.png");
+  const [login, setLogin] = useState<string>("Login do Usuário")
+  const [followers, setFollowers] = useState<number>(0);
+  const [following, setFollowing] = useState<number>(0);
+  const [company, setCompany] = useState<string>("Companhia do Usuário")
+  const [location, setLocation] = useState<string>("Localização do Usuário")
 
-  const handleSearch = () => {
+  const handleSearchUser = () => {
     axios
-      .get<GITHUBResponse>(`https://api.github.com/users/${userName}`)
+      .get<GitHubUser>(`https://api.github.com/users/${userName}`)
       .then((res) => {
         setName(res.data.name);
         setBio(res.data.bio);
         setAvatarURL(res.data.avatar_url);
+        setLogin(res.data.login);
+        setFollowers(res.data.followers)
+        setFollowing(res.data.following)
+        setCompany(res.data.company)
+        setLocation(res.data.location)
       })
       .catch((err) => {
         console.log(err);
@@ -35,12 +53,21 @@ function App() {
         <div className='container-busca'>
           <h3>Digite um usuário para buscar:</h3>
           <input type="text" onChange={(e) => setUserName(e.target.value)} />
-          <button onClick={handleSearch}>Procurar</button>
+          <button onClick={handleSearchUser}>Procurar</button>
         </div>
         <div className='container-usuario'>
           <img src={avatarURL} alt="Usuário" />
           <h1>{name}</h1>
-          <p>{bio}</p>
+          <p>{login}</p>
+          <p className='bio-usuário'>{bio}</p>
+          <p className='followers-following'><FaUser /> Seguidores: {followers} · Seguindo: {following}</p>
+          <p className='companhia-usuário'><FaBuilding /> {company}</p>
+          <p className='localização-usuário'><IoLocationSharp /> {location}</p>
+          <p className='repositório-usuário'>
+            <a target="_blank" href={`https://github.com/${userName}?tab=repositories`}>
+              <RiGitRepositoryFill /> Link do repositório do Usuário
+            </a>
+          </p>
         </div>
       </div>
     </div>
